2f1402c8de133584b9d3e248bb017a444d55d910
蒹葭采采 <70134597+gaoxuyang3073@users.noreply.github.com>
2023-01-31 13:41:39 +0800

    玩家数据类的重写

---
+++ reWrite02.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class reWrite02 {
+    public static void main(String[] args) {
+
+    }
+    class roundInf {
+
+    }
+    class player {
+        //角色信息
+        int mainRoleNumber;
+        String[] roleNameList = {"","",""};
+        int[] roleHPList = {10, 10, 10};
+        //玩家手上的资产
+        List<String> dice = new ArrayList<>();
+        //
+    }
+}
