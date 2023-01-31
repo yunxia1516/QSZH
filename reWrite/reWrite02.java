41ee055a081e9f5bbcc42dc975997cf754e66dc2
蒹葭采采 <70134597+gaoxuyang3073@users.noreply.github.com>
2023-01-31 18:40:58 +0800

    Update reWrite02.java

--- reWrite/reWrite02.java
+++ reWrite/reWrite02.java
@@ -5,18 +5,35 @@ import java.util.List;
 
 public class reWrite02 {
     public static void main(String[] args) {
-
     }
-    class roundInf {
-
-    }
-    class player {
-        //角色信息
-        int mainRoleNumber;
-        String[] roleNameList = {"","",""};
-        int[] roleHPList = {10, 10, 10};
-        //玩家手上的资产
-        List<String> dice = new ArrayList<>();
-        //
-    }
+}
+class player1 {
+    //角色信息
+    int mainRoleNumber;
+    String[] roleNameList = {"","",""};
+    int[] roleHPList = {10, 10, 10};
+    //角色攻击信息：变量名数字代表技能
+    int[] roleCombatSkillDice1 = {};
+    int[] roleCombatSkillDice2 = {};
+    int[] roleCombatSkillDice3 = {};
+    int[] roleCombatSkillHurtNumber1 = {};
+    int[] roleCombatSkillHurtNumber2 = {};
+    int[] roleCombatSkillHurtNumber3 = {};
+    //玩家资产
+    List<String> dice = new ArrayList<>();
+}
+class player2 {
+    //角色信息
+    int mainRoleNumber;
+    String[] roleNameList = {"","",""};
+    int[] roleHPList = {10, 10, 10};
+    //角色攻击信息：变量名数字代表技能
+    int[] roleCombatSkillDice1 = {};
+    int[] roleCombatSkillDice2 = {};
+    int[] roleCombatSkillDice3 = {};
+    int[] roleCombatSkillHurtNumber1 = {};
+    int[] roleCombatSkillHurtNumber2 = {};
+    int[] roleCombatSkillHurtNumber3 = {};
+    //玩家资产
+    List<String> dice = new ArrayList<>();
 }
